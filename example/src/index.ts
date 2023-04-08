7aac77303643c68d0fc33eeca6c1a07c92e74b64
Tobshub <tobanigeorge@gmail.com>
2023-04-08 19:38:47 +0100

    updated example

--- example/src/index.ts
+++ example/src/index.ts
@@ -6,6 +6,17 @@ app.use("/", (req, res) => {
   res.sendFile("index.html");
 });
 
+app.use("echo", (req, res) => {
+  const url = req.url.split("/").filter((path) => path !== "");
+  // remove echo from the text
+  url.shift();
+  if (url.length) {
+    res.send(url.join(" ").replace(/(%20)|\+/g, " "));
+    return;
+  }
+  res.send({ message: "Append text to the url to make me say stuff!" });
+});
+
 app.listen(4000, () => {
   console.log("LIVE port(4000)");
 });
