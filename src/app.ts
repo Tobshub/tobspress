6f2eaa0a9a9b8511c1bc02b44f8439ff787c203c
Tobshub <tobanigeorge@gmail.com>
2023-04-08 08:41:08 +0100

    changes

--- src/app.ts
+++ src/app.ts
@@ -4,30 +4,24 @@ const app = new Tobspress();
 
 app.static("public");
 
-app.use("echo", {
-  handler: async (req, res) => {
-    const url = req.url.split("/").filter((path) => path !== "");
-    // remove echo from the text
-    url.shift();
-    if (url.length) {
-      res.send(url.join(" ").replace(/(%20)|\+/g, " "));
-      return;
-    }
-    res.send({ message: "Append text to the url to make me say stuff!" });
-  },
+app.use("echo", async (req, res) => {
+  const url = req.url.split("/").filter((path) => path !== "");
+  // remove echo from the text
+  url.shift();
+  if (url.length) {
+    res.send(url.join(" ").replace(/(%20)|\+/g, " "));
+    return;
+  }
+  res.send({ message: "Append text to the url to make me say stuff!" });
 });
 
-app.post("form", {
-  handler: async (req, res) => {
-    console.log("body:", await req.body);
-    res.send(await req.body);
-  },
+app.post("form", async (req, res) => {
+  console.log("body:", await req.body);
+  res.send(await req.body);
 });
 
-app.use("/", {
-  handler: async (req, res) => {
-    res.sendFile("public/index.html");
-  },
+app.use("/", async (req, res) => {
+  res.sendFile("public/index.html");
 });
 
 app.listen(4000, () => console.log("live port(4000)"));
