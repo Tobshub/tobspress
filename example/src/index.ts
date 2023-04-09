2c3607a12aae9154bc30b1f753ff925ba9bba785
Tobshub <tobanigeorge@gmail.com>
2023-04-09 01:19:42 +0100

    improved example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,4 +1,4 @@
-import Tobspress from "@tobshub/tobspress";
+import Tobspress, { TobspressRouter } from "@tobshub/tobspress";
 
 const app = new Tobspress();
 
@@ -6,12 +6,22 @@ app.use("/", (req, res) => {
   res.sendFile("index.html");
 });
 
+const echoRouter = new TobspressRouter();
+echoRouter.use("/hello/world", (req, res) => {
+  res.send("Come on! That's too easy.");
+});
+
 app.use("echo", (req, res) => {
   const url = req.url.split("/").filter((path) => path !== "");
   // remove echo from the text
   url.shift();
   if (url.length) {
-    res.send(url.join(" ").replace(/(%20)|\+/g, " "));
+    const message = url.join(" ").replace(/(%20)|\+/g, " ");
+    if (message.toLowerCase() === "hello world") {
+      res.send("Come on! That's too easy.");
+      return;
+    }
+    res.send(message);
     return;
   }
   res.send({ message: "Append text to the url to make me say stuff!" });
