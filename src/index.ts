c2319879f8d2dcaa21a67afaf4b253d8c87d909e
Tobshub <tobanigeorge@gmail.com>
2023-04-10 20:17:42 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { Server, IncomingMessage, ServerResponse, createServer } from "http";
-import {
-  splitPath,
-  sanitizePath,
-  optionalExecute,
-  tobspressLog,
-} from "./helpers";
+import { splitPath, sanitizePath, optionalExecute, tobspressLog } from "./helpers";
 import { TobspressChildRouter, TobspressRouter } from "./router";
 import {
   type TobspressRouterType,
@@ -19,14 +14,7 @@ import {
 import path from "path";
 
 // export types that might me useful in API implementation
-export {
-  Method,
-  TobspressRequest,
-  TobspressResponse,
-  type TobspressRouterFn,
-  TobspressRouter,
-  type TobspressOptions,
-};
+export { Method, TobspressRequest, TobspressResponse, type TobspressRouterFn, TobspressRouter, type TobspressOptions };
 
 /** The Tobspress instance */
 class Tobspress {
@@ -73,32 +61,20 @@ class Tobspress {
     if (!url.length) {
       if (router && router.children && router.children.has({ path: "" })) {
         router = router.children.get({ path: "" });
-      } else if (
-        router &&
-        router.children &&
-        router.children.has({ path: "", method: request.method })
-      ) {
+      } else if (router && router.children && router.children.has({ path: "", method: request.method })) {
         router = router.children.get({ path: "", method: request.method });
       }
     } else {
       for (let i = 0; i < url.length; i++) {
         let path = url[i];
         searchPath = searchPath.length ? searchPath.concat("/", path) : path;
-        if (
-          router &&
-          router.children &&
-          router.children.has({ path: searchPath })
-        ) {
+        if (router && router.children && router.children.has({ path: searchPath })) {
           router = router.children.get({
             path: searchPath,
           });
           // reset searchPath
           searchPath = "";
-        } else if (
-          router &&
-          router.children &&
-          router.children.has({ path: searchPath, method: request.method })
-        ) {
+        } else if (router && router.children && router.children.has({ path: searchPath, method: request.method })) {
           router = router.children.get({
             path: searchPath,
             method: request.method,
@@ -121,8 +97,7 @@ class Tobspress {
     } else if (
       router &&
       router.children &&
-      (router.children.has({ path: "" }) ||
-        router.children.has({ path: "", method: request.method }))
+      (router.children.has({ path: "" }) || router.children.has({ path: "", method: request.method }))
     ) {
       // attempt to use child router on path "/" if router found but no handler
       const handler =
@@ -136,20 +111,13 @@ class Tobspress {
     }
 
     // as a last resort, treat the url as a static file path
-    const foundFile = await response.sendFile(
-      path.join(this.staticFolderPath, request.url)
-    );
+    const foundFile = await response.sendFile(path.join(this.staticFolderPath, request.url));
 
     if (foundFile) return;
 
     // return 404 if no handler or file is found
     response.status(404).send({ error: "NOT FOUND" });
-    this.log(
-      request.method,
-      "request done for:",
-      request.url,
-      foundFile && "not found"
-    );
+    this.log(request.method, "request done for:", request.url, !foundFile && "not found");
   }
 
   /** Attaches a non-method specific router */
@@ -182,28 +150,16 @@ class Tobspress {
     return this;
   }
 
-  private attachRouter(
-    method: Method | "USE",
-    path: string,
-    fn: TobspressRouterFn
-  ) {
+  private attachRouter(method: Method | "USE", path: string, fn: TobspressRouterFn) {
     if (typeof fn === "function") {
       this.children.set(
-        method === "USE"
-          ? { path: sanitizePath(path) }
-          : { path: sanitizePath(path), method },
+        method === "USE" ? { path: sanitizePath(path) } : { path: sanitizePath(path), method },
         new TobspressChildRouter(fn, undefined, [], method === "USE")
       );
     } else {
       this.children.set(
-        method === "USE"
-          ? { path: sanitizePath(path) }
-          : { path: sanitizePath(path), method },
-        new TobspressChildRouter(
-          fn.handler,
-          fn.router?.children,
-          fn.router?.middlewares
-        )
+        method === "USE" ? { path: sanitizePath(path) } : { path: sanitizePath(path), method },
+        new TobspressChildRouter(fn.handler, fn.router?.children, fn.router?.middlewares)
       );
     }
   }
