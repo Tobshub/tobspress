cc9c046f32fa516460bfd679cdc146040922ed35
Tobshub <tobanigeorge@gmail.com>
2023-05-11 03:20:53 +0100

    nested routes

--- example/src/api/router.ts
+++ example/src/api/router.ts
@@ -10,4 +10,14 @@ apiRouter
     res.send(`you sent: ${JSON.stringify((await req.body) ?? {})}`);
   });
 
+const deepRouter = new TobspressRouter();
+const deeperRouter = new TobspressRouter();
+const deepestRouter = new TobspressRouter();
+deepestRouter.use("exec", (_, res) => {
+  res.send({message: "this is the deepest router"});
+})
+deeperRouter.use("deepest", {router: deepestRouter})
+deepRouter.use("deeper", {router: deeperRouter});
+apiRouter.use("deep", {router: deepRouter})
+
 export default apiRouter;
